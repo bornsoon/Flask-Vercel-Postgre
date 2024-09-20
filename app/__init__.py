5581c8bd305925b34f73678e245818878e827f64
HL <rlimm2@knou.ac.kr>
2024-09-20 17:06:16 +0900

    Adding Files

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-2from flask import Flask
+from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://default:2JVBgN6fdZEi@ep-small-cell-a48v87ie-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://default:4T2xWfmzOYgZ@ep-shiny-lake-a4u2wx0x-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(24)  # or another method to set a secret key
