a21f5e313eeb60821ce56883b33fe5e0a3116604
HL <rlimm2@knou.ac.kr>
2024-09-21 19:32:52 +0900

    9/21

--- config.py
+++ config.py
@@ -2,5 +2,7 @@ import os
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://default:4T2xWfmzOYgZ@ep-shiny-lake-a4u2wx0x-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require')
+    
+    # 지워도 DB에 입력은 됨, 하지만 flash가 안뜨고 오류 500 발생.
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(24)  # or another method to set a secret key
